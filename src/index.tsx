f08b1eac9338e9ed7284bd1ba6c4da207662af84
Your Name <sekab>
2021-05-21 22:13:53 +0200

    feat: using Application Provider in the entry file

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApplicationProvider } from './state';
 
 ReactDOM.render(
   <React.StrictMode>
+      <ApplicationProvider>
     <App />
+    </ApplicationProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
