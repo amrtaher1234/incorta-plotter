b02ab64ae67df56ea0513a7fac3810293ff77bce
Your Name <sekab>
2021-05-22 18:33:47 +0200

    style: :art: adding interfaces for plots and nivo data

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,9 +1,21 @@
 export interface IColumnItem {
-    name: string;
-    function: IColumnItemFunction
+  name: string;
+  function: IColumnItemFunction;
 }
 
 export enum IColumnItemFunction {
-    dimension = 'dimension',
-    measure = 'measure'
+  dimension = "dimension",
+  measure = "measure",
+}
+
+export interface IPlotterData {
+  name: string;
+  values: string[] | number[];
+}
+
+export interface INivoLineData {
+  id: string;
+  color: string;
+  data: any[];
+  legends: string[];
 }
