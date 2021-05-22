9be3d41f1559d4dee5dfe48032450c89abc88a99
Your Name <sekab>
2021-05-22 18:49:51 +0200

    style: :lipstick: styling column button wrapper

--- src/components/ColumnButtonWrapper/index.tsx
+++ src/components/ColumnButtonWrapper/index.tsx
@@ -8,11 +8,13 @@ import {
 } from "@material-ui/core";
 import React from "react";
 import { useDrop } from "react-dnd";
+import SpeedIcon from "@material-ui/icons/Speed";
+import TimelineIcon from "@material-ui/icons/Timeline";
 import { IColumnItem, IColumnItemFunction } from "../../models";
 
 export interface ColumnButtonWrapperInterface {
   columnItems: IColumnItem[];
-  type: IColumnItemFunction | string;
+  type: IColumnItemFunction;
   onClear: () => void;
   onDeleteItem: (item: IColumnItem) => void;
   onItemAdded: (item: IColumnItem) => void;
@@ -36,6 +38,11 @@ const useStyles = makeStyles((theme: Theme) =>
       alignItems: "center",
       minHeight: 55,
     },
+    buttonText: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-evenly",
+    },
     chip: {
       margin: theme.spacing(0.5),
     },
@@ -46,7 +53,20 @@ export default function ColumnButtonWrapper(
 ) {
   const { columnItems, type, onClear, onDeleteItem, onItemAdded } = props;
   const classes = useStyles();
-
+  const getButtonText = () => {
+    if (type === IColumnItemFunction.measure) {
+      return (
+        <>
+          <SpeedIcon /> {type}{" "}
+        </>
+      );
+    }
+    return (
+      <>
+        <TimelineIcon /> {type}{" "}
+      </>
+    );
+  };
   const [{ canDrop, isOver }, drop] = useDrop(() => ({
     accept: props.type,
     drop: (data: any) => {
@@ -65,7 +85,9 @@ export default function ColumnButtonWrapper(
 
   return (
     <div className={classes.root}>
-      <Typography variant="body1">{type}</Typography>
+      <Typography className={classes.buttonText} variant="body1">
+        {getButtonText()}
+      </Typography>
       <Paper ref={drop} component="ul" className={classes.paper}>
         {columnItems.map((item, index) => {
           return (
