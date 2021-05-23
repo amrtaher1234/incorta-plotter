5a4fc9e9bdfb159682b85f69bd2d71efe3fd68a8
Your Name <sekab>
2021-05-23 03:19:54 +0200

    adding logo and favicon

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,27 @@
-import React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import logo from "./../../assets/logo.png"; // Tell webpack this JS file uses this image
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       flexGrow: 1,
     },
+    logo: {
+      width: 40,
+      height: 40,
+      marginRight: 10,
+    },
     toolbar: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        background: 'black'
-    }
-  }),
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      background: "black",
+    },
+  })
 );
 
 export default function Header() {
@@ -24,7 +30,8 @@ export default function Header() {
     <div className={classes.root}>
       <AppBar position="relative">
         <Toolbar className={classes.toolbar}>
-            <Typography variant="h6">Incorta Plotter</Typography>
+          <img className={classes.logo} src={logo} />
+          <Typography variant="h6">Incorta Plotter</Typography>
         </Toolbar>
       </AppBar>
     </div>
